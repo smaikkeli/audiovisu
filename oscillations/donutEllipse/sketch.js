37c2931a7f0eefc7dc196dff544f562d0883df15
schmaigul <miksuhok@gmail.com>
2023-11-20 12:22:09 +0200

    donut ellipse a lot of customization

--- oscillations/donutEllipse/sketch.js
+++ oscillations/donutEllipse/sketch.js
@@ -3,45 +3,56 @@ let currentAngle = 0;
 let noiseOffset = 0;
 let removing = false;
 
+//Donut parameters
+let radiusScale = 40;
+let noiseIncrement = 0.02;
+let angleStep = 0.02;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800,800);
   noFill();
   colorMode(HSB);
   startNewDonut();
-  frameRate(30);
+  frameRate(35);
 }
 
 function draw() {
-  background(30);
+  background(25);
   translate(width / 2, height / 2);
 
   // Calculate wobble effect
-  let wobbleX = noise(frameCount * 0.001) * 20 - 10; // Horizontal wobble
-  let wobbleY = noise(frameCount * 0.001) * 20 - 10; // Vertical wobble
-  let wobbleRotation = noise(frameCount * 0.002) * 5; // Rotation wobble
+  //let wobbleX = noise(frameCount * 0.01) * 20 - 10; // Horizontal wobble
+  //let wobbleY = noise(frameCount * 0.01) * 20 - 10; // Vertical wobble
 
   // Apply wobble transformation
-  translate(wobbleX, wobbleY);
+  //translate(wobbleX, wobbleY);
+
+  let wobbleRotation = noise(frameCount * 0.002) * 5; // Rotation wobble
   rotate(wobbleRotation);
   
 
-  let donutRadius = 100; // The overall radius of the donut
+  let donutRadius = 150; // The overall radius of the donut
   let baseEllipseRadius = 10; // The base radius of each individual ellipse
-  let angleStep = 0.02; // The increment in angle for each frame
 
   if (!removing) {
     if (currentAngle < TWO_PI) {
-      let ellipseRadius = baseEllipseRadius + noise(noiseOffset)*40;
+       // Interpolate radius for a smooth transition in size
+      // Noise for variation
+      let noiseValue = noise(noiseOffset) * radiusScale;
+      // Sine wave for looping
+      let sineValue = (sin(currentAngle * 2) * 0.5 + 0.5);
+      // Combine both effects
+      let ellipseRadius = baseEllipseRadius + noiseValue * sineValue;
+
       let x = (donutRadius + cos(currentAngle) * ellipseRadius) * cos(currentAngle);
       let y = (donutRadius + cos(currentAngle) * ellipseRadius) * sin(currentAngle);
-
       // Dynamic color based on angle
       // Dynamic color based on angle within the new hue range
       let colorHue = map(currentAngle, 0, TWO_PI, startHue, endHue);
 
       ellipses.push({ x: x - ellipseRadius, y: y, radius: ellipseRadius, color: color(colorHue, 100, 60) });
       currentAngle += angleStep;
-      noiseOffset += 0.02; // Increment noise offset
+      noiseOffset += noiseIncrement; // Increment noise offset
     } else {
       removing = true;
     }
@@ -55,8 +66,14 @@ function draw() {
 
   for (let e of ellipses) {
     stroke(e.color); // Use the stored color for each ellipse
-    ellipse(e.x, e.y, e.radius * 2, e.radius * 2);
+    //ellipse(e.x, e.y, e.radius * 2, e.radius * 2);
+    jitter = random(1, 50);
+    distortedEllipse(e.x, e.y, e.radius * 2, e.radius * 2, jitter);
+
   }
+
+  //Randomly can switch from not removing to removing and vice versa
+
 }
 
 function startNewDonut() {
@@ -64,6 +81,35 @@ function startNewDonut() {
   currentAngle = 0;
   removing = false;
   ellipses = [];
+
+  //Paremeters for the donut
+  noiseIncrement = random(0.01, 0.04);
+  radiusScale = random(20,100);
+  //Choose angle step based on a probability distributrion with high density on lower values and lower density on higher values
+  angleStep = random([0.01, 0.01, 0.01, 0.02, 0.02, 0.05, , 0.05, 0.1, 0.2, 1]);
+
+  strokeWeight(random([0.1, 0.2, 0.6, 1, 2]));
   startHue = random(360); // Starting hue
   endHue = (startHue + random(60, 180)) % 360; // Ending hue within a range of 60 to 180 degrees from the start hue
+  noiseOffset = random(1000); // Reset noiseOffset for variety
+
+}
+
+function distortedEllipse(x, y, w, h) {
+  push(); // Save current drawing state
+  translate(x, y);
+
+  // Simulate hand-drawn effect
+  let jitter = 50;
+  beginShape();
+  for (let angle = 0; angle < TWO_PI; angle += 0.1) {
+    let jitterX = noise(angle, frameCount * 0.01) * jitter - jitter / 2;
+    let jitterY = noise(frameCount * 0.01, angle) * jitter - jitter / 2;
+    let sx = cos(angle) * w / 2 + jitterX;
+    let sy = sin(angle) * h / 2 + jitterY;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
+
+  pop(); // Restore original drawing state
 }
